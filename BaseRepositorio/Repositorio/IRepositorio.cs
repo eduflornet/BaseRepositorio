a7d657c4b946080744fee6d6db6324d26fb04e2f
eduflormir <eduflormir@gmail.com>
2015-11-10 22:12:01 +0100

    Repository Pattern
    El patrón Repositorio, ViewModel, Implementación práctica con Entity
    Framework

---
+++ BaseRepositorio/Repositorio/IRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BaseRepositorio.ViewModel;
+
+namespace BaseRepositorio.Repositorio
+{
+    public interface IRepositorio<TModelo, TViewModel> where TModelo : class
+                         where TViewModel : IViewModel<TModelo>
+    {
+        TViewModel Add(TViewModel model);
+        int Borrar(TViewModel model);
+        int Borrar(Expression<Func<TModelo, bool>> consulta);
+        int Actualizar(TViewModel model);
+        ICollection<TViewModel> Get();
+        TViewModel Get(params Object[] keys);
+        ICollection<TViewModel> Get(Expression<Func<TModelo, bool>> consulta);
+
+    }
+
+}
---
+++ BaseRepositorio/ViewModel/IViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseRepositorio.ViewModel
+{
+    public interface IViewModel<TModelo> where TModelo : class
+    {
+        TModelo ToBaseDatos();
+        void FromBaseDatos(TModelo modelo);
+        void UpdateBaseDatos(TModelo modelo);
+        object[] GetKeys();
+    }
+}
